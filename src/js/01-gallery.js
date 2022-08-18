038a28540fdf5d68b3101337ced6b7c5f406c3c7
zlatan-dp <zlatandnepr@gmail.com>
2022-08-18 13:07:39 +0300

    gallery done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryImageItems = galleryItems
+  .map(
+    galleryItem => `<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>`
+  )
+  .join('');
+
+galleryContainer.innerHTML = galleryImageItems;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+document.addEventListener('click', lightbox);
