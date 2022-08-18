f5d52e384ac82c2ee92ea2e05f2aa7fc7662fe01
zlatan-dp <zlatandnepr@gmail.com>
2022-08-18 15:08:01 +0300

    part og 02video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+// import player from '@vimeo/player';
 
+// const iframe = document.querySelector('iframe');
+// const iframePlayer = new Vimeo.Player(iframe);
+
+// iframePlayer.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// iframePlayer.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+console.log('hello');
