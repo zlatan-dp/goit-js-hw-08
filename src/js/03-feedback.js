f8d7d1c2c98db8174b9d566078404d8434a05658
zlatan-dp <zlatandnepr@gmail.com>
2022-08-19 15:50:49 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+const form = document.querySelector('.feedback-form');
+const FEEDBACK_FORM_KEY = 'feedback-form-state';
 
+form.addEventListener('input', saveData);
+//form.addEventListener('submit', submitData);
+
+populateForm();
+
+const formData = {};
+
+function saveData(evt) {
+  evt.preventDefault();
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(FEEDBACK_FORM_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+  const savedData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_KEY));
+  if (savedData) {
+    //console.log(savedData.email);
+    form.email.value = savedData.email;
+    form.message.value = savedData.message;
+  }
+}
