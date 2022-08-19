8c50cd4ca6d933119e47152b6d41b2ba6a8c07e2
zlatan-dp <99171484+zlatan-dp@users.noreply.github.com>
2022-08-19 13:26:52 +0000

    Deploying to gh-pages from @ zlatan-dp/goit-js-hw-08@665db9b2175e178843682d613e7a9c56b2e35aca 🚀

--- 03-feedback.671ea22e.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");e.addEventListener("input",(function(e){e.preventDefault(),t[e.target.name]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(t))})),function(){const t=JSON.parse(localStorage.getItem("feedback-form-state"));t&&(e.email.value=t.email,e.message.value=t.message)}();const t={};
-//# sourceMappingURL=03-feedback.671ea22e.js.map
